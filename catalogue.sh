8b5faaa5892721c0ae8689c03254bb307fa906b8
Saideepika <912183@cognizant.com>
2023-05-17 19:50:57 +0530

    Catalogue and redis script changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,35 @@
+echo -e "\e[36m>>>>>Configuring NodeJS repos<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>Install NodeJS<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>Create Application Directory<<<<<\e[0m"
 useradd roboshop
+rm -rf /app
 mkdir /app 
+
+echo -e "\e[36m>>>>>Downlaod App Content<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
+
+echo -e "\e[36m>>>>>Unzip App content<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+
+echo -e "\e[36m>>>>>Install Nodejs Dependencies<<<<<\e[0m"
 npm install 
-cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>Copy catalogue system file <<<<<\e[0m"
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>Start catalogue system<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl start catalogue
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>Copy MongoDB repo <<<<<\e[0m"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>Install MongoDB client<<<<<\e[0m"
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.rsaideepika123.online </app/schema/catalogue.js
--- redis.sh
+++ redis.sh
@@ -1,8 +1,14 @@
+echo -e "\e[36m>>>>>Install rpms repo files<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m>>>>>Install Redis<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
 yum install redis -y 
 
 ## File to be edit listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf
+echo -e "\e[36m>>>>>Change port number<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 
+echo -e "\e[36m>>>>>Redis systrem restart<<<<<\e[0m"
 systemctl enable redis 
 systemctl start redis 
